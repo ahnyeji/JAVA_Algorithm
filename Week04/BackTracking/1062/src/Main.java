3940a6b7a00d29f8668a077a15082d4539987706
ahnyeji <yejian1218@naver.com>
2021-02-16 02:27:25 +0900

    15.FEB.2021 / 가르침 (16.FEB modified)

--- Week04/BackTracking/1062/src/Main.java
+++ Week04/BackTracking/1062/src/Main.java
@@ -1,19 +1,14 @@
-// 미완성 코드
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
-    static String[] words;
-    static ArrayList<HashMap<Character, Boolean>> alphas= new ArrayList<>();
-    static boolean[] alphabet = new boolean[26];
+    static HashMap[] wordAlphas; // alphabets in words
     static int maxW = 0;
-    static int lastAlpha = 0;
+    static int maxKey = 19; // last alphabet of alphabets in all words
+    static int[] alphabet = new int[26]; // 0 : never used, 1 : used in words but not checked, 2 : checked
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -22,73 +17,84 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        words = new String[N];
-
+        wordAlphas = new HashMap[N];
+        char[] s;
+        int len, key;
         for(int i = 0; i < N; i++){
-            words[i] = in.readLine();
-            words[i] = words[i].substring(4, words[i].length() - 4);
-        }
+            s = in.readLine().toCharArray();
+            len = s.length;
+            wordAlphas[i] = new HashMap<>();
+            for(int j = 0; j < len; j++){
+                key = s[j] - 'a'; // alphabet to number(index)
+                maxKey = Math.max(maxKey, key); // to know last(largest index) alphabet
+                switch (key){ // set "anta" & "tica" learned (to make not to check again)
+                    case 0: // 'a'
+                    case 2: // 'c'
+                    case 8: // 'i'
+                    case 13: // 'r'
+                    case 19: // 't'
+                        wordAlphas[i].put(key, true);
+                        alphabet[key] = 2;
+                        break;
+                    default:
+                        wordAlphas[i].put(key, false);
+                        alphabet[key] = 1;
+                        break;
+                }
 
-        if(K < 6){
+            }
+        }
+        maxKey++;
+
+        if(K < 5){ // cannot learn "anta" & "tica" -> cannot learn any word
             System.out.println(0);
             return;
         }
 
-        char[] se = {'a', 'n', 't', 'i', 'c'};
-        for(char c : se){
-            alphabet[c - 'a'] = true;
+        int tolearn = 0; // count of alphabets to learn to read all words
+        for(int i = 0; i < 26; i++){
+            if(alphabet[i] > 0) tolearn++;
         }
-
-        for(String word : words){
-            int len = word.length();
-            HashMap<Character, Boolean> alphaW = new HashMap<>();
-            for(int i = 0; i < len; i++){
-                alphaW.put(word.charAt(i), false);
-                lastAlpha = Math.max(lastAlpha, word.charAt(i) - 'a');
-            }
-            alphaW.remove('a');
-            alphaW.remove('n');
-            alphaW.remove('t');
-            alphaW.remove('i');
-            alphaW.remove('c');
-
-            alphas.add(alphaW);
+        if(K >= tolearn){ // can learn every alphabet to read all words
+            System.out.println(N);
+            return;
         }
-        lastAlpha++;
-
-        for(HashMap<Character, Boolean>)
-
-        teaching(5);
+        teaching(5, 1); // 5 : already learned 5 alphabet, 1 : start from 'b'
         System.out.println(maxW);
     }
 
-    static void teaching(int cnt){
+    static void teaching(int cnt, int idx){ // cnt : count of alphabet learned, idx : starting point of new alphabet
         if(cnt == K){
             int cntW = 0;
-            for(HashMap<Character, Boolean> afterCheck : alphas){
-                if(!afterCheck.containsValue(false)){
+
+            for(int i = 0; i < N; i++){
+                if(!wordAlphas[i].containsValue(false)){ // can read this word
                     cntW++;
                 }
             }
             maxW = Math.max(maxW, cntW);
             return;
         }
-        if(maxW == N) return;
-        for(int i = 0; i < lastAlpha; i++){
-            if(!alphabet[i]){
+        if(maxW == N) return; // no need to learn more
+        for(int i = idx; i < maxKey; i++){
+            if(alphabet[i] == 1){ // used but not checked
+                alphabet[i] = 2;
+
                 for(int j = 0; j < N; j++){
-                    HashMap<Character, Boolean> word = alphas.get(j);
-                    if(word.containsKey('a' + i)){
-                        word.put((char) ('a' + i), true);
+                    if(wordAlphas[j].containsKey(i)){
+                        wordAlphas[j].put(i, true); // true : can read (learned)
                     }
                 }
-                teaching(cnt + 1);
+
+                teaching(cnt + 1, i + 1);
+
                 for(int j = 0; j < N; j++){
-                    HashMap<Character, Boolean> word = alphas.get(j);
-                    if(word.containsKey('a' + i)){
-                        word.put((char) ('a' + i), false);
+                    if(wordAlphas[j].containsKey(i)){
+                        wordAlphas[j].put(i, false); // false : cannot read (not learned)
                     }
                 }
+
+                alphabet[i] = 1;
             }
         }
     }
