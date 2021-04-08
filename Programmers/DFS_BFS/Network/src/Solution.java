5b803c94c7cdcb7fd628f566178810eb3ec06b73
ahnyeji <yejian1218@naver.com>
2021-04-08 19:19:03 +0900

    08.APR.2021 / 네트워크

---
+++ Programmers/DFS_BFS/Network/src/Solution.java
@@ -0,0 +1,36 @@
+/*  Union-Find version */
+
+class Solution {
+    int[] p;
+    public int solution(int n, int[][] computers) {
+        p = new int[n + 1];
+        for(int i = 1; i <= n; i++) p[i] = i;
+        int len = computers.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(computers[i][j] == 0) continue;
+                if(find(i + 1) == find(j + 1)) continue;
+                union(i + 1, j + 1);
+            }
+        }
+        int answer = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i == p[i]) answer++;
+        }
+        return answer;
+    }
+
+    public int find(int x) {
+        if(x == p[x]) return x;
+        return p[x] = find(p[x]);
+    }
+
+    public void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return;
+        if(x < y) p[y] = x;
+        else p[x] = y;
+    }
+}
